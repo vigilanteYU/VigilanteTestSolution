91ab606f22bcf7e6450bd1dfb7a2f5b62a68273f
vigilanteYU <vigilante.yukkuri@gmail.com>
2022-03-18 17:25:29 +0900

    CSVHelperの読み込みサンプル追加

---
+++ CSVHelperTest/CSVTest.cs
@@ -0,0 +1,12 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CSVHelperTest
+{
+    public class CSVTest
+    {
+        [Name("id")]
+        public string Id { get; set; }
+        [Name("name")]
+        public string Name { get; set; }
+    }
+}
--- CSVHelperTest/Program.cs
+++ CSVHelperTest/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CsvHelper;
+using CSVHelperTest;
+using System.Globalization;
+
+using (var reader = new StreamReader(@".\test.csv"))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    var records = csv.GetRecords<CSVTest>();
+    foreach (var record in records)
+    {
+        Console.WriteLine($"{record.Id},{record.Name}");
+    }
+}
